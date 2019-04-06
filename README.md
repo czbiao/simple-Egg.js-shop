e3aff46d98d4e7db86d5e06826ee68f67fc08832
chenzebiao <13413423055@163.com>
2019-04-06 16:28:04 +0800

    add Migrations database
    add Migrations database

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 simple-egg-shop api
 
+
+# 数据库操作
+执行 migrate 进行数据库变更
+### 升级数据库
+npx sequelize db:migrate
+### 如果有问题需要回滚，可以通过 `db:migrate:undo` 回退一个变更
+npx sequelize db:migrate:undo
+### 可以通过 `db:migrate:undo:all` 回退到初始状态
+npx sequelize db:migrate:undo:all
+
 ## QuickStart
 
 <!-- add docs here for user -->
---
+++ database/migrations/20190406081321-init-users.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    // 在执行数据库升级时调用的函数，创建 users 表
+    const { INTEGER, DATE, STRING } = Sequelize;
+    await queryInterface.createTable('users', {
+      id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+      name: STRING(30),
+      age: INTEGER,
+      created_at: DATE,
+      updated_at: DATE,
+    });
+  },
+
+  // 在执行数据库降级时调用的函数，删除 users 表
+  down: async (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    await queryInterface.dropTable('users');
+  },
+};
