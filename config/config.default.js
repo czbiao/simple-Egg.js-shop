64e327a309548dec68a034fcfc9a46bd4caeb252
chenzebiao <13413423055@163.com>
2019-04-06 16:09:29 +0800

    add egg-sequelize
    add egg-sequelize

--- config/config.default.js
+++ config/config.default.js
@@ -18,5 +18,14 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [ 'errorHandler' ];
 
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'wechat_shop',
+    username: 'root',
+    password: '123456',
+  };
+
   return config;
 };
