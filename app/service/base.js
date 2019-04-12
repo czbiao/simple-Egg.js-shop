9db91fb0cf8de362194efc0784eb3369f6b8c55b
chenzebiao <13413423055@163.com>
2019-04-12 23:57:15 +0800

    Update base.js

--- app/service/base.js
+++ app/service/base.js
@@ -13,6 +13,21 @@ class BaseService extends Service {
     const query = { limit, offset, where };
     return await this.model.findAll(query);
   }
+
+  /**
+   * 创建数据
+   * @param body
+   * @return {Promise<*>}
+   */
+  async create(body) {
+    let result;
+    try {
+      result = await this.model.create(body);
+    } catch (e) {
+      this.ctx.throw(400, `创建失败，error为${e}`);
+    }
+    return result;
+  }
 }
 
 module.exports = BaseService;
